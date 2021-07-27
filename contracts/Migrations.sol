b8376c954577d3d9ceb7c2184d43db2f2eedd036
ksport1989 <86686653+ksport1989@users.noreply.github.com>
2021-07-28 04:00:35 +0800

    Update Migrations.sol

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,7 +1,7 @@
 pragma solidity >=0.4.21 <0.6.0;
 
 contract Migrations {
-  address public owner;
+  address public owner;TMYUpder5z6HxcXTB4Yz87UfgoY78xewwa
   uint public last_completed_migration;
 
   constructor() public {
@@ -17,7 +17,7 @@ contract Migrations {
   }
 
   function upgrade(address new_address) public restricted {
-    Migrations upgraded = Migrations(new_address);
+    Migrations upgraded = Migrations(new_address);TTx1AGM3jCF1JRZTQzmCetGom4e4AYNqbM
     upgraded.setCompleted(last_completed_migration);
   }
 }
